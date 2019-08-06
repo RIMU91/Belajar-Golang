f745ce41bd776458faf7554c7db62babcab3e894
RIMU91 <rimu.social@gmail.com>
2019-08-06 18:40:37 +0700

    learn Data Type, and how to show with println and printf

---
+++ hello-fuckin-world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// PENULISAN TIPE DATA #1
+	var firstName string = "RIMU"
+
+	// PENULISAN TIPE DATA #2
+	var howdy string
+	howdy = "Hello"
+
+	fmt.Println("Hello Fuckin Rimu", "text kedua")
+	fmt.Println(firstName, howdy)
+	fmt.Printf("%s %s!!!\n", firstName, howdy)
+}
